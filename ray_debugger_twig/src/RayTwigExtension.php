ec749f3d1e50f6ea0d89154df46f3e83dc040651
Sean Montague <you@example.com>
2022-04-28 15:41:21 -0400

    RayTwigExtension

--- ray_debugger_twig/src/RayTwigExtension.php
+++ ray_debugger_twig/src/RayTwigExtension.php
@@ -1,15 +1,19 @@
 <?php
-namespace Drupal\ray_debugger_twig;
+namespace Drupal\ray_debugger;
 
 use Twig\Extension\AbstractExtension;
-
-use Twig\TwigFilter;
 use Twig\TwigFunction;
+
+
 
 /**
  * Custom twig Ray Debugger.
+ * )
  */
 class RayTwigExtension extends AbstractExtension {
+
+  // Public Methods
+  // =========================================================================
 
   /**
    * @inheritdoc
@@ -30,47 +34,5 @@ class RayTwigExtension extends AbstractExtension {
       }),
     ];
   }
-
-//  public function call($name, $arguments) {
-//    if (!count($arguments)) {
-//      return ray()->$name();
-//    }
-//
-//    return ray()->$name($arguments);
-//  }
-
-//  public function getTokenParsers() {
-//    $parser = new RayTokenParser();
-//    ksm($parser);
-//    return [
-//      new RayTokenParser(),
-//    ];
-//  }
-
-  /**
-   * @inheritdoc
-   */
-  public function getName() {
-    return 'Ray';
-  }
-
-  /**
-   * @inheritdoc
-   */
-//  public function getFilters() {
-//    return [
-//      new TwigFilter('ray', function ($params) {
-//        return ray_filter($params);
-//      }),
-//    ];
-//  }
-
-//  public function ray($params) {
-//    ray($params);
-//  }
-
-
-
-
-
 }
+
